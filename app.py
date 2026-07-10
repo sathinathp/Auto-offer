ba38e5e1b10d6bfe8d4c542173602c4dadc85bca
sathinathp <sathinath.padhi@petabytz.com>
2026-07-10 22:32:25 +0530

    Fix: server port back to 5001 with debug mode

--- app.py
+++ app.py
@@ -362,5 +362,5 @@ def email_offer():
     })
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=True)
