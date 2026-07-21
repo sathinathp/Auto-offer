95b42e0070609cda57e2fd6a3e0f7a5c8f269bb0
sathinathp <sathinath.padhi@petabytz.com>
2026-07-21 18:33:34 +0530

    Optimize Page 1 whitespace, update Page 5 Annexure table layout and header background color to #a9d08e

--- app.py
+++ app.py
@@ -390,5 +390,5 @@ def email_offer():
         return jsonify({'status': 'error', 'message': f'PDF Generation Failed: {str(e)}'}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5001))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=True)
